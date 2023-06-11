9ffbf062e6c1902a47ff9358783021e4fae88b57
서현우 <shw1801@naver.com>
2023-06-11 17:18:56 +0900

    [Bronze V] Title: 사칙연산, Time: 60 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -0,0 +1,13 @@
+A, B = map(int, input().split())
+
+sum = A + B
+minus = A - B
+multiple = A * B
+division = A // B
+remainder = A % B
+
+print(sum)
+print(minus)
+print(multiple)
+print(division)
+print(remainder)
